2fad51bc7f8561a94a5757f38e3d7ce749e089ab
kensei.o <kensei.o@gmail.com>
2018-10-01 11:30:35 +0900

    change for raspi

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import json
 
 # TODO:リクエストエラーのときの処理を考える
 r = requests.get('http://traininfo.jreast.co.jp/train_info/kanto.aspx')
-soup = BeautifulSoup(r.text, 'lxml')
+soup = BeautifulSoup(r.text, 'html.parser')
 
 
 # TODO:横須賀線は[2]で決め打ちじゃなくて検索して探す。
 train = soup.find_all('th', class_='text-tit-xlarge')[2]
 train_name = train.text
-print(train_name)
+# print(train_name)
 train_situation = train.find_next_sibling('td', class_='acess_i').find('img').attrs['alt']
-print(train_situation)
+# print(train_situation)
 
 train_dict = {}
 train_dict['tarin_name'] = train_name
@@ -29,9 +29,9 @@ if train_situation != '平常運転':
         except:
             delivery_time = ""
 
-    print(delivery_time)
+    # print(delivery_time)
     cause = train.parent.find_next_sibling('tr').find('td').text
-    print(cause)
+    # print(cause)
     train_dict['delivery_time'] = delivery_time
     train_dict['couse'] = cause
 
@@ -39,12 +39,13 @@ with open('info.json') as f:
     data = json.load(f)
 
 before = data['train_situation']
-print(before)
+# print(before)
 print(str(before) == str(train_situation))
 
 
 # GET前と後で値が変わってたらファイル更新＆LINEに通知
 if train_situation != before:
+    print('execute notifire')
     with open('info.json', 'w') as f:
         json.dump(train_dict, f)
 
